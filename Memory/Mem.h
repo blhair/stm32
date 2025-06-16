d6b50937527c730c018b2f43850275fc6b9a4532
hust-y <3266456420@qq.com>
2025-06-16 20:24:02 +0800

    重构了我的mem库，增强了它的可复用性

--- Memory/Mem.h
+++ Memory/Mem.h
@@ -9,21 +9,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_MEM_NUM 10
+// Maximum number of memory blocks, adjust as needed.
+#define MAX_MEM_SIZE 1000 
+// Maximum memory size, adjust as needed. 
+//But when you reset it, please reset the minimum heap size in the STM32CubeMX to a larger value, 
+//otherwise you will get NULL pointer.
+
 typedef struct{
-	double *lData;
-	double *rData;
+	double *Data;
 	int size;
 	int div;
 	int total;
+	short isSet;
+	//isSet is used to indicate whether the memory block has been initialized with data.
+	//If isSet is 0, it means the memory block has not been initialized with data.
 	UART_HandleTypeDef *huart;
 }Mem;
+
+typedef float (*MemFunc)(void);
+typedef float (*MemFuncSeq)(int);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void SpeedMemInit(int size, int div, UART_HandleTypeDef *huart);
-void MemOut(void);
-void destoryMem(void);
+void MemInit(Mem *m, int size, int div, UART_HandleTypeDef *huart);
+void MemInitDefault(int size, int div, UART_HandleTypeDef *huart);
+void MemSet(Mem *m, double (*func)(void));
+double MemGet(Mem *m, int i);
+MemFunc WrapFunc(MemFuncSeq func, int i);
+void MemSetSequence(int begin, int end, double (*func)(int), int bi);
+void MemOut(Mem *m);
+void MemOutDefault(void);
+void destoryMem(Mem *m);
+void destoryMemDefault(void);
 
+// ...existing code...
 #endif // MEM_H
